da898493986525728a3d7993cee92cf88d80a4f7
Syzygianinfern0 <spsharan2000@gmail.com>
2022-06-03 21:40:28 +0530

    Shell check, IDE ignores

--- arch_install.sh
+++ arch_install.sh
@@ -9,23 +9,23 @@ timedatectl set-ntp true
 lsblk
 echo "Enter the drive: "
 read drive
-cfdisk $drive 
+cfdisk $drive
 echo "Enter the linux partition: "
 read partition
-mkfs.ext4 $partition 
+mkfs.ext4 $partition
 read -p "Did you also create efi partition? [y/n]" answer
-if [[ $answer = y ]] ; then
-  echo "Enter EFI partition: "
-  read efipartition
-  mkfs.vfat -F 32 $efipartition
+if [[ $answer = y ]]; then
+    echo "Enter EFI partition: "
+    read efipartition
+    mkfs.vfat -F 32 $efipartition
 fi
-mount $partition /mnt 
+mount $partition /mnt
 pacstrap /mnt base base-devel linux linux-firmware
-genfstab -U /mnt >> /mnt/etc/fstab
-sed '1,/^#part2$/d' `basename $0` > /mnt/arch_install2.sh
+genfstab -U /mnt >>/mnt/etc/fstab
+sed '1,/^#part2$/d' $(basename $0) >/mnt/arch_install2.sh
 chmod +x /mnt/arch_install2.sh
 arch-chroot /mnt ./arch_install2.sh
-exit 
+exit
 
 #part2
 printf '\033c'
@@ -33,55 +33,55 @@ pacman -S --noconfirm sed
 sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf
 ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
 hwclock --systohc
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "en_US.UTF-8 UTF-8" >>/etc/locale.gen
 locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-echo "KEYMAP=us" > /etc/vconsole.conf
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+echo "KEYMAP=us" >/etc/vconsole.conf
 echo "Hostname: "
 read hostname
-echo $hostname > /etc/hostname
-echo "127.0.0.1       localhost" >> /etc/hosts
-echo "::1             localhost" >> /etc/hosts
-echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
+echo $hostname >/etc/hostname
+echo "127.0.0.1       localhost" >>/etc/hosts
+echo "::1             localhost" >>/etc/hosts
+echo "127.0.1.1       $hostname.localdomain $hostname" >>/etc/hosts
 mkinitcpio -P
 passwd
 pacman --noconfirm -S grub efibootmgr os-prober
-echo "Enter EFI partition: " 
+echo "Enter EFI partition: "
 read efipartition
 mkdir /boot/efi
-mount $efipartition /boot/efi 
+mount $efipartition /boot/efi
 grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB
 sed -i 's/quiet/pci=noaer/g' /etc/default/grub
 sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
 grub-mkconfig -o /boot/grub/grub.cfg
 
 pacman -S --noconfirm xorg-server xorg-xinit xorg-xkill xorg-xsetroot xorg-xbacklight xorg-xprop \
-     noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome \
-     sxiv mpv zathura zathura-pdf-mupdf ffmpeg imagemagick  \
-     fzf man-db xwallpaper python-pywal unclutter xclip maim \
-     zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl  \
-     dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse \
-     emacs-nox arc-gtk-theme rsync qutebrowser dash \
-     xcompmgr libnotify dunst slock jq aria2 cowsay \
-     dhcpcd connman wpa_supplicant rsync pamixer mpd ncmpcpp \
-     zsh-syntax-highlighting xdg-user-dirs libconfig \
-     bluez bluez-utils
+    noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-jetbrains-mono ttf-joypixels ttf-font-awesome \
+    sxiv mpv zathura zathura-pdf-mupdf ffmpeg imagemagick \
+    fzf man-db xwallpaper python-pywal unclutter xclip maim \
+    zip unzip unrar p7zip xdotool papirus-icon-theme brightnessctl \
+    dosfstools ntfs-3g git sxhkd zsh pipewire pipewire-pulse \
+    emacs-nox arc-gtk-theme rsync qutebrowser dash \
+    xcompmgr libnotify dunst slock jq aria2 cowsay \
+    dhcpcd connman wpa_supplicant rsync pamixer mpd ncmpcpp \
+    zsh-syntax-highlighting xdg-user-dirs libconfig \
+    bluez bluez-utils
 
-systemctl enable NetworkManager.service 
+systemctl enable NetworkManager.service
 rm /bin/sh
 ln -s dash /bin/sh
-echo "%wheel ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "%wheel ALL=(ALL) NOPASSWD: ALL" >>/etc/sudoers
 echo "Enter Username: "
 read username
 useradd -m -G wheel -s /bin/zsh $username
 passwd $username
 echo "Pre-Installation Finish Reboot now"
 ai3_path=/home/$username/arch_install3.sh
-sed '1,/^#part3$/d' arch_install2.sh > $ai3_path
+sed '1,/^#part3$/d' arch_install2.sh >$ai3_path
 chown $username:$username $ai3_path
 chmod +x $ai3_path
 su -c $ai3_path -s /bin/sh $username
-exit 
+exit
 
 #part3
 printf '\033c'
